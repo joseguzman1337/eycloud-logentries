752350a68d1b78ab66ef7167e9fdfbda27a1d14f
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:51:18 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- cookbooks/le/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: le
-# Recipe:: default
-#
-require_recipe 'le::install'
-require_recipe 'le::configure'
-require_recipe 'le::start'
--- cookbooks/le/recipes/start.rb
+++
@@ -1,9 +0,0 @@
-
-# Cookbook Name:: le
-# Recipe:: start
-#
-
-# Restart the le agent
-execute 'start le agent' do
-	command %{/etc/init.d/logentries restart}
-end
